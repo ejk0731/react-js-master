21912c2afc9d8d6c4068bf883e891c6b31f26c1a
ejk0731 <103646256+ejk0731@users.noreply.github.com>
2024-02-08 15:21:03 +0900

    add: React Basic #6.0~6.3 - Effects

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,43 @@
-import Button from './Button';
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log("i run only once");
+  }, []);
+
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.")
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("i run when 'counter' changes.")
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("i run when 'keyword & counter' changes.")
+  }, [keyword, counter]);
+
   return (
     <div className="App">
-      <p className={styles.title}>Welcome Back!</p>
-      <Button text={"button"}/>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <p>{counter}</p>
+      <button onClick={onClick}>Click Me!</button>
     </div>
   );
 }
