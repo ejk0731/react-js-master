fd52a7c260c285ecc0a13c2529eebc1724b90eb1
ejk0731 <103646256+ejk0731@users.noreply.github.com>
2024-02-22 14:49:37 +0900

    add: React Basic #7.2 - Practice Movie App (Coin Tracker)

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,40 +1,59 @@
-import { useState } from "react";
-/* 
-  state Change - two ways
-  * 직접 값 넣어주기
-  * 함수 넣어주기
-*/
+import { useEffect, useState } from "react";
+
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo] );
-    setToDo("");
-  }
-  console.log(toDos);
+  const [isLoading, setIsLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [budget, setBudget] = useState("");
+  const [dollarForCoin, setDollarForCoin] = useState("");
+  const [count, setCount] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=100")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    setIsLoading(false);
+  }, []);
+
+  const changeBudgetVal = (e) => {
+    setBudget(e.target.value);
+  };
+  const changeCoinOption = (e) => {
+    setDollarForCoin(e.target.value);
+  };
+  const handleCoinChanger = () => {
+    setCount(Math.floor(budget / dollarForCoin))
+  };
+  
   return (
-    <div>
-      <h1>My To Do ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write what to do..!"></input>
-        <button type="submit">Add to do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, i) => {
-          return(
-            <li key={i}>{item.toUpperCase()}</li>
-          )
-        })}
-      </ul>
-    </div>
+    <>
+      <h1>The Coins! ({coins.length})</h1>
+      {isLoading ? (
+        <span>loading...</span>
+      ) : (
+        <div>
+          <input
+            type="text"
+            value={budget}
+            onChange={changeBudgetVal}
+            id="money"
+            placeholder="Show me what you got!"
+          />
+          <select id="coinList" onChange={changeCoinOption}>
+            {coins.map((coin, i) => {
+              return (
+                <option key={i} value={coin.quotes.USD.price}>
+                  {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+                </option>
+              );
+            })}
+          </select>
+          <button type="submit" onClick={handleCoinChanger}>
+            How many coins can i buy?
+          </button>
+          <p> {count} {count > 1 ? "coins" : "coin"} </p>
+        </div>
+      )}
+    </>
   );
 }
 
