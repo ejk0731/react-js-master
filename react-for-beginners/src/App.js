e7ac103686ba77fba3625d29acd5604d93a75a47
ejk0731 <103646256+ejk0731@users.noreply.github.com>
2024-02-27 19:12:46 +0900

    add: React Basic #7.5 - Practice Movie App

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,12 +1,29 @@
 /* 
   React Router
   npm install react-router-dom
+
+  BrowserRouter - http://localhost:3001/
+  HashRouter - http://localhost:3001/#/
+  Link - 브라우저 새로고침 없이도 유저를 다른 페이지로 이동시켜주는 컴포넌트
 */
 
+import { BrowserRouter as  Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie" element={ <Detail />}>
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
